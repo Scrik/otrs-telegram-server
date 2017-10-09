3145bf38ced2fd45a8c541712fc8b9af9c6e8d5d
Eduardo Almeida <eduardo.almeida@tijucaalimentos.com.br>
2017-10-09 11:19:48 -0300

    Create a Constructor

--- routes/notes.js
+++ routes/notes.js
@@ -13,10 +13,11 @@ router.get('/:id', function(req, res, next) {
     OTRS.getTicket(data.TicketID).then((Ticket) => {
         data.telegram.chatID = Ticket.DynamicField[0].Value;
         data.telegram.body = Ticket.Article[0].Body;
-        request.get(`https://api.telegram.org/bot457248917:AAHTK6Ec5gLbuTj5lvFKyL6hlGZEPGhpozQ/sendMessage?chat_id=${data.telegram.chatID}&text=${encodeURIComponent(data.telegram.body)}`,(err,res,body) => {
+        // request.get(`https://api.telegram.org/bot457248917:AAHTK6Ec5gLbuTj5lvFKyL6hlGZEPGhpozQ/sendMessage?chat_id=${data.telegram.chatID}&text=${encodeURIComponent(data.telegram.body)}`,(err,res,body) => {
             // console.log(data.telegram.body);
-            console.log(data,`https://api.telegram.org/bot457248917:AAHTK6Ec5gLbuTj5lvFKyL6hlGZEPGhpozQ/sendMessage?chat_id=${data.telegram.chatID}&text=${encodeURIComponent(data.telegram.body)}`);
-        });
+            // console.log(data,`https://api.telegram.org/bot457248917:AAHTK6Ec5gLbuTj5lvFKyL6hlGZEPGhpozQ/sendMessage?chat_id=${data.telegram.chatID}&text=${encodeURIComponent(data.telegram.body)}`);
+        // });
+        console.log(data);
     }).catch((err) => {
         console.log(err);
     })
