6a51b3112e445b83c79bbcf8f52c4f698009c7b8
Eduardo Almeida <eduardo.almeida@tijucaalimentos.com.br>
2017-10-13 12:58:52 -0300

    New Note

--- routes/notes.js
+++ routes/notes.js
@@ -11,13 +11,20 @@ router.get('/:id', function(req, res, next) {
     data.query = req.query;
     data.telegram = {};
     OTRS.getTicket(data.TicketID).then((Ticket) => {
+
         data.telegram.chatID = Ticket.DynamicField[0].Value;
+
         data.telegram.body = Ticket.Article[0].Body;
-        data.telegram.message = `${Ticket.Article[0].FromRealname} (Atendente):\n${Ticket.Article[0].Title}\n\n"${Ticket.Article[0].Body}"`
-    
-        request.get(`https://api.telegram.org/bot457248917:AAHTK6Ec5gLbuTj5lvFKyL6hlGZEPGhpozQ/sendMessage?chat_id=${data.telegram.chatID}&text=${encodeURIComponent(data.telegram.message)}`,(err,res,body) => {
-            console.log(data.telegram.body);
-        });
+
+        data.telegram.message = `${Ticket.Article[0].FromRealname} (Atendente):\n[TICKET#${Ticket.Article[0].TicketNumber}|${Ticket.Article[0].TicketID}]\n\nResposta: ${Ticket.Article[0].Body}`;
+        
+        if(Ticket.Article[0].FromRealname != "Telegram BOT"){
+            request.get(`https://api.telegram.org/bot457248917:AAHTK6Ec5gLbuTj5lvFKyL6hlGZEPGhpozQ/sendMessage?chat_id=${data.telegram.chatID}&text=${encodeURIComponent(data.telegram.message)}`,(err,res,body) => {
+                console.log(data.telegram.body);
+            });
+        }
+
+       
     }).catch((err) => {
         console.log(err);
     })
