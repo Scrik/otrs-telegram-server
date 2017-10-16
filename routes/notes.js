c13167a60a938b7e24b2d172bb5fb8e6b8d35c82
Eduardo Almeida <eduardo.almeida@tijucaalimentos.com>
2017-10-16 15:15:00 -0300

    Prod

--- routes/notes.js
+++ routes/notes.js
@@ -19,7 +19,7 @@ router.get('/:id', function(req, res, next) {
         data.telegram.message = `${Ticket.Article[0].FromRealname} (Atendente):\n[TICKET#${Ticket.Article[0].TicketNumber}|${Ticket.Article[0].TicketID}]\n\nResposta: ${Ticket.Article[0].Body}`;
         
         if(Ticket.Article[0].FromRealname != "Telegram BOT"){
-            request.get(`https://api.telegram.org/bot457248917:AAHTK6Ec5gLbuTj5lvFKyL6hlGZEPGhpozQ/sendMessage?chat_id=${data.telegram.chatID}&text=${encodeURIComponent(data.telegram.message)}`,(err,res,body) => {
+            request.get(`https://api.telegram.org/bot445113487:AAE8FET984QpOTsLIqEMZTMpRH2NUkAO6v4/sendMessage?chat_id=${data.telegram.chatID}&text=${encodeURIComponent(data.telegram.message)}`,(err,res,body) => {
                 console.log(data.telegram.body);
             });
         }
